7163ac3d80801bcf7ea4f3cc6b35ecf580a634bb
kjangeti <karthik.jangeti533@gmail.com>
2017-06-06 19:49:05 +0530

    code adjustments for s3 path update

--- app/config/constants.php
+++ app/config/constants.php
@@ -197,8 +197,8 @@ return [
     'S3BUCKET_CAMPAIGN_IMAGES' => 'mintmesh/dev/campaignImages',
     'S3BUCKET_COMPANY_IMAGES' => 'mintmesh/dev/companyImages',
     'S3BUCKET_FILE' =>  'mintmesh/dev/files',
-    'UPLOAD_RESUME' => '/uploads/awss3_resumes/',
-    'S3_DOWNLOAD_PATH' => 'https://s3-us-west-2.amazonaws.com/mintmeshresumedev/',
+    'UPLOAD_RESUME'  => '/uploads/awss3_resumes/',
+    'S3_DOWNLOAD_PATH'  => 'https://s3-us-west-2.amazonaws.com/mintmeshresumedev/',
     'BITLY_URL'   =>   'https://api-ssl.bitly.com/v3/user/link_save?access_token=',
     'BITLY_ACCESS_TOKEN' => 'b65a3bf8c767c8931eeaa067da88c2e2356f192e'
         
