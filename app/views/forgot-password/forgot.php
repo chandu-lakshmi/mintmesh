bac13cf04aaeda412784b73fe7b0ff153c5969dc
jayakrishana <t.jayakrishna@enterpi.com>
2017-07-05 12:24:41 +0530

    change

--- app/views/forgot-password/forgot.php
+++ app/views/forgot-password/forgot.php
@@ -2,6 +2,7 @@
 <html lang="en" ng-app="myApp" ng-cloak>
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Forgot Password</title>
         <link rel="stylesheet" type="text/css" href="<?php echo url('/'); ?>/app/views/forgot-password/css/style.css" />
         <link rel="stylesheet" type="text/css" href="<?php echo url('/'); ?>/app/views/forgot-password/css/bootstrap.css" />
@@ -34,9 +35,6 @@
                                         <div class="camp_logo">
                                             <img src="<?php echo url('/'); ?>/app/views/forgot-password/images/company_logo.svg" alt="logo">
                                         </div>
-                                        <div class="forgot-password-header">
-                                            <p>We will send an email to your registered email address with instructions to help you reset your password</p>
-                                        </div>
                                         <div class="forgot-password-content">
                                             <form role="form" name="re_enter_password_form" ng-submit="forgotCtrl.forgot_submit(re_enter_password_form.$valid)" novalidate>
                                                 <div class="error-spacer" style="float:none">
@@ -48,7 +46,7 @@
                                                 <div>
                                                     <md-input-container class="md-block">
                                                         <label for="new_password">New Password</label>
-                                                        <input type="password" id="new_password" name="new_password" ng-model="forgotCtrl.forgotPassword.new_password" required ng-pattern="/^(?=.*[a-zA-Z0-9]).{6,16}$/" ng-class="{'has-error-border':re_enter_password_form.new_password.$invalid && forgotCtrl.forgot_show_error}">
+                                                        <input type="password" id="new_password" name="new_password" ng-model="forgotCtrl.forgotPassword.new_password" required ng-pattern="/^(?=.*[a-zA-Z0-9]).{6,}$/" ng-class="{'has-error-border':re_enter_password_form.new_password.$invalid && forgotCtrl.forgot_show_error}">
                                                         <div ng-if="re_enter_password_form.new_password.$error.required && forgotCtrl.forgot_show_error" class="has-error-msg">Please Enter Password.</div>
                                                         <div ng-if="re_enter_password_form.new_password.$error.pattern && forgotCtrl.forgot_show_error" class="has-error-msg">Please Enter Atleast 6 characters.</div>
                                                     </md-input-container>
@@ -68,7 +66,7 @@
                                     <div class="verification-box zoomIn animated" ng-if="!forgotCtrl.initStep">
                                         <div class="verification-box-content text-center">
                                             <img src="<?php echo url('/'); ?>/app/views/forgot-password/images/tickmark.svg" alt="tick">
-                                            <p class="text" ng-bind="forgetPwdCtrl.backendMsg"></p>
+                                            <p class="text" ng-bind="forgotCtrl.backendMsg"></p>
 <!--                                            <button class="btn btn-style" style="width:40%" ng-click="forgetPwdCtrl.signin()">OK</button>-->
                                         </div>
                                     </div>
