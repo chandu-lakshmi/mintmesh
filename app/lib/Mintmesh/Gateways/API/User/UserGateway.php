ee110efbf622a26185bf17f211b2069acfde84fa
kjangeti <j.karthik@enterpi.com>
2017-07-24 19:22:21 +0530

    code adjustments for ticket 15068 issue

--- app/lib/Mintmesh/Gateways/API/User/UserGateway.php
+++ app/lib/Mintmesh/Gateways/API/User/UserGateway.php
@@ -1645,19 +1645,13 @@ class UserGateway {
         
         public function getUserProfile()
         {
-//            if (Cache::has('userprofile')) { 
-//                $loggedinUserDetails = Cache::get('userprofile_'.Crypt::encrypt($this->loggedinUserDetails->emailid));                
-//                \Log::info("<<<<<<<<< In if >>>>>>>>>");
-//            } else {
-//                $loggedinUserDetails = $this->getLoggedInUser();
-//                \Log::info("<<<<<<<<< In else >>>>>>>>>");
-//                Cache::add('userprofile_'.Crypt::encrypt($this->loggedinUserDetails->emailid), $loggedinUserDetails, 1000);                
-//            }  
-//            
+       
             $responseMessage = $responseCode = $responseStatus = "";
             $responseData = array();
             $loggedinUserDetails = $this->getLoggedInUser();
-            $userId         = !empty($loggedinUserDetails->id)?$loggedinUserDetails->id:'';
+            $userId         = !empty($loggedinUserDetails->id) ? $loggedinUserDetails->id : '';
+            $firstName      = !empty($loggedinUserDetails->firstname) ? $loggedinUserDetails->firstname : '';
+            $lastName       = !empty($loggedinUserDetails->lastname) ? $loggedinUserDetails->lastname : '';
             #log user activity here
             $this->userRepository->addUserActivityLogs($userId, $appType=1, $moduleType=7);
             if ($loggedinUserDetails)
@@ -1665,6 +1659,7 @@ class UserGateway {
                 $requestsCount = 0;
                 $extraDetails = array();
                 $neoLoggedInUserDetails = $this->neoUserRepository->getNodeByEmailId($loggedinUserDetails->emailid) ;
+                
                 $moreDetails = $this->neoUserRepository->getMoreDetails($loggedinUserDetails->emailid);
                 if (!empty($moreDetails))
                 {
@@ -1684,6 +1679,9 @@ class UserGateway {
                 if (!empty($neoLoggedInUserDetails))
                 {
                     $r = $this->formUserDetailsArray($neoLoggedInUserDetails);
+                    $r['firstname'] = $firstName;
+                    $r['lastname']  = $lastName;
+                    
                     if (!empty($neoLoggedInUserDetails->cv_path) && !empty($neoLoggedInUserDetails->cv_renamed_name))
                     {
                         $r['cv_path'] = $neoLoggedInUserDetails->cv_renamed_name ;
