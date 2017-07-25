0b4b6bdbecc1be5952242911559f53df742434b1
jayakrishana <t.jayakrishna@enterpi.com>
2017-07-25 15:11:19 +0530

    trim the name in email templates

--- app/views/emails/Api/User/job_invitation.php
+++ app/views/emails/Api/User/job_invitation.php
@@ -26,7 +26,7 @@
                                                     <tr height="10px"><td>&nbsp;</td></tr>
                                                     <?php if(!empty($email_template)){?>
                                                     <tr>
-                                                        <td style="color:#000;padding: 5px 0;">Hello <?php echo $name;?>,<br /> <br />
+                                                        <td style="color:#000;padding: 5px 0;">Hello <?php echo trim($name);?>,<br /> <br />
 														You have been Referred by your friend <?php echo $fromName ; ?> for the job <?php echo $looking_for ; ?> at <?php echo $company_name ; ?></td>
                                                     </tr>
                                                     <?php } ?>
